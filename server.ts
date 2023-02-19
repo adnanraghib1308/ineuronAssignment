a28461b1b8e3d9eebf12b838d6b5acdc3b0a5119
Adnan Raghib <adnanraghib@Adnans-MacBook-Air.local>
2023-02-19 16:38:04 +0530

    initial commit

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ const bodyParser = express.json({});
 
 app.use(bodyParser);
 
-app.get('/', (req: Request, res: Response) => { res.send("Hi there V5!!") });
+app.get('/', (req: Request, res: Response) => { res.send("Hi there V6!!") });
 app.use('/api', require('./controllers'));
 
 app.listen(3030, () => console.log("Server running on port 3030"));
